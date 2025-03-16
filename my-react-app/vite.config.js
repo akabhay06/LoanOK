9f6322a15534fa81909d2090ed7d72926361842c
abhay2602 <97271757+abhay2602@users.noreply.github.com>
2025-03-16 23:11:22 +0530

    base

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,16 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       '@': '/src',
     },
-  },
-  server: {
-    host: true,
-    strictPort: true,
-    cors: true,  // Enable CORS
-    origin: "https://d054-2409-40d6-1010-db96-a135-7f67-701f-3ba.ngrok-free.app", // Allow Ngrok URL
-    allowedHosts: ["d479-59-96-46-44.ngrok-free.app"], // Add your blocked Ngrok host here
   },
 });
